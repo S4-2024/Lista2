50c08620501fc688d14cfc229253d26c97a98d44
Gabi <gabrielle.sote@gmail.com>
2024-05-09 16:46:38 -0300

    a

---
+++ src/Induction.java
@@ -0,0 +1,5 @@
+public class Induction {
+
+    int numero;
+    
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+// Nome: Gabrielle Soares Teixeira - 422221763
+// Data: 04/05/2024
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
